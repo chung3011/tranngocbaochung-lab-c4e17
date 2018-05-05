177aaf41c57de1a738a546a3a53a965387e193f5
chung3011 <chung.hitc97@gmail.com>
2018-05-05 16:13:02 +0700

    ccc

--- lab2/homework/vnm.py
+++ lab2/homework/vnm.py
@@ -7,30 +7,38 @@ conn = urlopen(url)
 raw_data=conn.read()
 text = raw_data.decode("utf8")
 
+# f = open("vnm.html","wb")
+# f.write(raw_data)
+# f.close()
+
 soup = BeautifulSoup(text,"html.parser")
-# header
-tblGridData=soup.find("table",id='tblGridData')
-td_list = tblGridData.find_all("td")
-header_list=[]
-for td in td_list:
-    header = td.string
-    header_list.append(header)
+
+# # header
+# tblGridData=soup.find("table",id='tblGridData')
+# td_list = tblGridData.find_all("td")
+# header_list=['']
+# for td in td_list:
+#     if td and td.string is not None:
+#         header = td.string.strip()
+#         header_list.append(header)
+# header_list.remove("Tăng trưởng")
 # print(header_list)
 
 # content
 tableContent = soup.find("table",id="tableContent")
-td_list=tableContent.find_all("td")
-content_list=[]
-for td in td_list:
-    name_content = td.string
-    content_list.append(name_content)
-# print(content_list)
+tr_list=tableContent.find_all("tr")
 table=[]
-for i in range(len(header_list)-1):
-    for j in range(i,len(content_list),13):
-        dict={
-        header_list[i]:content_list[j]
-        }
-        table.append(dict)
+
+for tr in tr_list:
+    td_list=tableContent.find_all("td")
+    data={}
+    if td_list and td_list[0].string is not None:
+        dict['Title'] = (td_list[0].string.strip())
+        dict['4-2016'] = (td_list[1].string)
+        dict['1-2017'] = (td_list[2].string)
+        dict['2-2017'] = (td_list[3].string)
+        dict['3-2017'] = (td_list[4].string)
+    if dict:
+        table.append(data)
 
 pyexcel.save_as(records=table,dest_file_name="vnm.xlsx")
